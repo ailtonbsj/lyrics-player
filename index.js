14be6235030aff94566e8126d94820519f046665
ailtonbsj <ailton.ifce@gmail.com>
2019-12-16 03:14:50 -0300

    create struture of app

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-// Youtube IFrame API
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '360', width: '640',
-        videoId: '',
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
-
-function onPlayerReady(event) {
-}
-
-function onPlayerStateChange(event) {
-}
-
 // Lyric Text
-var lyricByTime = [];
+var lyricByTime;
 var progress;
+var display;
+var lyricMusic;
 function getLyric(url, onReadyCallback) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let lrc = this.responseText;
+            lyricByTime = [];
             lrc.split("\n").map(function (line) {
                 let div = line.lastIndexOf(']');
                 let command = line.substring(0, div + 1);
@@ -52,29 +31,29 @@ function getLyric(url, onReadyCallback) {
         }
     };
 
-    xhttp.open("GET", url, true);
+    xhttp.open("GET", `lyrics/${url}.lrc`, true);
     xhttp.send();
 }
 
 function playWithLyric() {
     player.playVideo();
-    play.innerText = "Pause";
+    //play.innerText = "Pause";
     progress = setInterval(progressVideo, 500);
 }
 
 function pauseWithLyric() {
     player.pauseVideo();
-    play.innerText = "Play";
+    //play.innerText = "Play";
     clearInterval(progress);
 }
 
 var lastLine = -1;
 function progressVideo() {
     lyricByTime.every(function (line, i) {
-        if (line.stamp > (player.getCurrentTime()+5)) {
+        if (line.stamp > (player.getCurrentTime())) {
             if (lastLine != i) {
                 display.innerHTML += "<br/>" + line.text;
-                display.scrollTo(0, display.scrollHeight);
+                lyricMusic.scrollTo(0, display.scrollHeight);
                 lastLine = i;
             }
             return false;
@@ -82,25 +61,35 @@ function progressVideo() {
     });
 }
 
-var display;
+function loadNewMusic(videoId, lyricName) {
+    pauseWithLyric();
+    display.innerHTML = "";
+    player.loadVideoById({ 'videoId': videoId });
+    pauseWithLyric();
+    getLyric(lyricName, function () {
+        playWithLyric();
+    });
+}
+
+function getListOfMusic() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let data = JSON.parse(this.responseText);
+            let list = document.getElementById("list-lyrics");
+            data.map(function(item){
+                list.innerHTML += `<li onclick="loadNewMusic('${item.id}','${item.lyric}')">${item.lyric}</li>`;
+            });
+        }
+    };
+
+    xhttp.open("GET", "data.json", true);
+    xhttp.send();
+}
+
 window.onload = function () {
-    var play = document.getElementById("play");
-    var add = document.getElementById("add");
-    display = document.getElementById("display");
-    add.onclick = function () {
-        player.loadVideoById({ 'videoId': 'E_mU-Z-bKbw' });
-        pauseWithLyric();
-        getLyric("Evanescence Missing.lrc", function () {
-            playWithLyric();
-        });
-    }
-    play.onclick = function () {
-        if (play.innerText == "Play") {
-            playWithLyric();
-        }
-        else {
-            pauseWithLyric();
-        }
-    }
     document.getElementById("player").style.display = "none";
+    display = document.getElementById("display");
+    lyricMusic = document.getElementById("lyric-music");
+    getListOfMusic();
 }
---
+++ youtubeapi.js
@@ -0,0 +1,23 @@
+// Youtube IFrame API
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360', width: '640',
+        videoId: '',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+}
+
+function onPlayerStateChange(event) {
+}
